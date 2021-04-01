46ca360f05ce6b08d29a3cde932939e542227146
felix13 <29225465_rogitofelix_29225465>
2021-04-01 09:41:04 +0300

    first commit

---
+++ movies/templatetags/custom_filter.py
@@ -0,0 +1,22 @@
+from django import template
+
+
+register = template.Library()
+
+
+
+def human_format(num):
+    decimal_format  = '{:.2f}'
+    int_num = int(num)
+    if int_num > 999 and int_num < 1000000:
+        return decimal_format.format(int_num/1000.0).rstrip('0').rstrip('.') + 'K' 
+    if int_num > 1000000:
+        return decimal_format.format(int_num/1000000.0).rstrip('0').rstrip('.') + 'M'
+    elif int_num < 1000:
+        return str(int_num)
+        
+        
+        
+        
+
+register.filter('human_format',  human_format )
